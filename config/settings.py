be4b4e4402897ddc4a8731a400ef596c51495999
xddongx <rlaehdgus21@hanmail.net>
2022-08-19 14:57:16 +0900

    postgresql 테스트

--- config/settings.py
+++ config/settings.py
@@ -80,6 +80,16 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
+    # test DB
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'wanted',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'ddong1234',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 
